16ff5c85e0ff848a824908c9401fe6886ed97bda
Andre Dragacevac <a.dragacevac@icloud.com>
2022-10-18 08:33:16 -0400

    commit tree 892f0d550fc6e907679131c261dfcc755b5b5e50

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution here
+      const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+      function findMatching(drivers, string) {
+        return drivers.filter((driver)=>{
+             return driver.toLowerCase() === string.toLowerCase() })
+      }
+
+      function fuzzyMatch(drivers, string){
+        return drivers.filter((driver)=>{
+            return driver.toLowerCase().substring(0, string.length) === string.toLowerCase() })
+        }
+    function matchName(drivers, argument){
+        return drivers.filter((driver)=>{
+            return driver.name === argument})
+        }
